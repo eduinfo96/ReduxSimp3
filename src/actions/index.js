3403c1ae05ed9bccb7c83126768e853db7d148fc
Theodrick Beck <tbeck@nectarom.com>
2017-03-06 10:33:18 -0600

    Finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = 'ff1300ac4f5f9f762023077528ec0d4f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${ API_KEY }`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather( city ){
+  const url = `${ ROOT_URL }&q=${ city },us`;
+  const request = axios.get( url );
+
+  return{
+    type: FETCH_WEATHER
+    , payload: request
+  };
+}
